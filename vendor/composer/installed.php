b84f011046f1441bd3eff88278e6f0ed15d71010
cleverdevelopper <cleverdevelopper@gamil.com>
2025-09-24 15:36:02 +0200

    intitial commit

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'socialmedia/socialmedia',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '753cfa0b271046b5b049271d4f1f2294420368ff',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'socialmedia/socialmedia' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '753cfa0b271046b5b049271d4f1f2294420368ff',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
